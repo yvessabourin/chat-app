b9878053234d4e3c11338424988b0b4545a07d22
Yves Sabourin <yvessabourin@MacBook-Pro-de-Yves.local>
2017-11-21 17:05:37 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+// console.log(__dirname + './../public');
+// console.log(publicPath);
+
+
+app.listen(port, () => {
+    console.log(`Express server is listening on port ${port}`);
+});
